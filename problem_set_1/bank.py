ae6a6be390392b91cafb13b4a24efd2497f8994c
Erebor <chris.hinson@proton.me>
2024-06-13 13:25:43 -0400

    file system restructured

--- problem_set_1/bank.py
+++ problem_set_1/bank.py
@@ -1,9 +1,12 @@
-if __name__ == "__main__":
-    greeting = input("Greeting: ").lstrip()
+def main ():
+    greeting = input("Greeting: ").strip()
 
-    if greeting.lower() == "hello":
+    if greeting.lower().startswith("hello"):
         print("$0")
-    elif greeting[0].lower() == "h":
+    elif greeting.lower().startswith("h"):
         print("$20")
     else:
         print("$100")
+
+if __name__ == "__main__":
+    main()
